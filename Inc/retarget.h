8ddc70919595192b85caff22b77ca00b4a77f496
LIAN <17600978517@163.com>
2023-04-15 16:03:55 +0800

    LMT70

--- Inc/retarget.h
+++ Inc/retarget.h
@@ -1,9 +1,8 @@
 #ifndef _RETARGET_H__
 #define _RETARGET_H__
-#include "stm32f4xx_it.h"
+#include "stm32f4xx_hal.h"
 #include <sys/stat.h>
 #include <stdio.h>
-
 void RetargetInit(UART_HandleTypeDef *huart);
 
 int _isatty(int fd);
