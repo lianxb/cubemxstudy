5cdf95abb70b4a78a319a400f097a3f1e998e310
LIAN <17600978517@163.com>
2023-04-16 13:11:41 +0800

    LMT70

--- Src/main.c
+++ Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "retarget.h"
+#include "adc.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
