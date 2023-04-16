a740a9aeeab38ac6f12b89558a02d86b506c3bfe
LIAN <17600978517@163.com>
2023-04-16 16:53:52 +0800

    LMT70

--- Drivers/MyDrivers/Src/adc.c
+++ Drivers/MyDrivers/Src/adc.c
@@ -19,18 +19,26 @@ ADC_HandleTypeDef ADC1_Handler;//ADC句柄
 //返回值:转换结果
 uint16_t Get_Adc(uint32_t ch) {
     ADC_ChannelConfTypeDef ADC1_ChanConf;
-
-    ADC1_ChanConf.Channel = ch;                                   //通道
-    ADC1_ChanConf.Rank = 1;                                       //第1个序列，序列1
-    ADC1_ChanConf.SamplingTime = ADC_SAMPLETIME_480CYCLES;        //采样时间
+    //通道
+    ADC1_ChanConf.Channel = ch;
+    //第1个序列，序列1
+    ADC1_ChanConf.Rank = 1;
+    //采样时间
+    ADC1_ChanConf.SamplingTime = ADC_SAMPLETIME_480CYCLES;
     ADC1_ChanConf.Offset = 0;
-    HAL_ADC_ConfigChannel(&ADC1_Handler, &ADC1_ChanConf);        //通道配置
-
-    HAL_ADC_Start(&ADC1_Handler);                               //开启ADC
-
-    HAL_ADC_PollForConversion(&ADC1_Handler, 10);                //轮询转换
-
-    return (uint16_t) HAL_ADC_GetValue(&ADC1_Handler);            //返回最近一次ADC1规则组的转换结果
+    //通道配置
+    HAL_ADC_ConfigChannel(&ADC1_Handler, &ADC1_ChanConf);
+    //开启ADC
+    HAL_ADC_Start(&ADC1_Handler);
+    //轮询转换
+    HAL_ADC_PollForConversion(&ADC1_Handler, 10);
+    //判断转换是否完成
+    if (HAL_IS_BIT_SET(HAL_ADC_GetState(&ADC1_Handler), HAL_ADC_STATE_REG_EOC)) {
+        //返回最近一次ADC1规则组的转换结果
+        return (uint16_t) HAL_ADC_GetValue(&ADC1_Handler);
+    }
+    //ADC读取失败，返回0xFFFF
+    return UINT16_MAX;
 }
 
 //获取指定通道的转换值，取times次,然后平均
@@ -40,7 +48,12 @@ uint16_t Get_Adc_Average(uint32_t ch, uint8_t times) {
     uint32_t temp_val = 0;
     uint8_t t;
     for (t = 0; t < times; t++) {
-        temp_val += Get_Adc(ch);
+        uint16_t adc_val = Get_Adc(ch);
+        //ADC读取失败，返回0xFFFF
+        if(adc_val == UINT16_MAX) {
+            return UINT16_MAX;
+        }
+        temp_val += adc_val;
         HAL_Delay(5);
     }
     return temp_val / times;
